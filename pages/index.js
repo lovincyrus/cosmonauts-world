18514f72b6642850de2e1cadf5cf5ffdf103790b
Cyrus Goh <hello@lovincyrus.com>
2019-11-11 14:20:14 -0800

    Add new page for github

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ const Home = () => (
     <div className='py-10 p-5 mx-auto wrapper'>
       <h1 className='subpixel-antialiased font-sans text-lg text-gray-800 text-left font-bold text-2xl'>Cosmos &amp; Tendermint Ecosystem</h1>
       <p className="pt-1 subpixel-antialiased max-w-full text-gray-600 text-xl">Note: List of active projects in the cosmos ecosystem are curated best to the knowledge of the community. Everyone should maintain caution and exercise due diligence when researching new projects beyond provided informations.</p>
-      <p className="pt-2 text-gray-500">Built by <a href="https://github.com/lovincyrus">@lovincyrus</a></p>
+      <p className="pt-2 text-gray-500">Built by <a href="https://github.com/lovincyrus" target="blank_" rel="noopener noreferrer">@lovincyrus</a></p>
 
       <Companies />
     </div>
