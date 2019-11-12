b90093bc91ba557a1b29195ccc711662c9d0552d
Cyrus Goh <hello@lovincyrus.com>
2019-11-12 00:47:05 -0800

    Fix a tag

--- components/companies.jsx
+++ components/companies.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/button-has-type */
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
--- pages/projects/[post].jsx
+++ pages/projects/[post].jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
 import Link from 'next/link';
 import Head from 'next/head';
