d72ec72a8f1377aeeba9af844afc81c8e05445a1
Cyrus Goh <hello@lovincyrus.com>
2019-11-13 15:06:17 -0800

    Add simple bg container to index

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,29 +11,41 @@ const Home = () => (
     </Head>
 
     <div className="py-10 p-5 mx-auto max-w-6xl">
-      <h1 className="subpixel-antialiased font-sans text-lg text-gray-800 text-left font-bold text-2xl">
-        Cosmos &amp; Tendermint Ecosystem
-      </h1>
-      <p className="pt-1 subpixel-antialiased max-w-full text-gray-600 text-xl">
-        Note: List of active projects in the cosmos ecosystem are curated best
-        to the knowledge of the community. Everyone should maintain caution and
-        exercise due diligence when researching new projects beyond provided
-        informations.
-      </p>
-      <p className="pt-2 text-gray-500">
-        Built by
-        {' '}
-        <a
-          href="https://github.com/lovincyrus"
-          target="blank_"
-          rel="noopener noreferrer"
-        >
-          @lovincyrus
-        </a>
-      </p>
+      <div className="bg-container">
+        <h1 className="subpixel-antialiased font-sans text-lg text-gray-800 text-left font-bold text-2xl">
+          Cosmos &amp; Tendermint Ecosystem
+        </h1>
+        <p className="pt-1 subpixel-antialiased max-w-full text-gray-600 text-xl">
+          Note: List of active projects in the cosmos ecosystem are curated best
+          to the knowledge of the community. Everyone should maintain caution and
+          exercise due diligence when researching new projects beyond provided
+          informations.
+        </p>
+        <p className="pt-2 text-gray-500">
+          Built by
+          {' '}
+          <a
+            href="https://github.com/lovincyrus"
+            target="blank_"
+            rel="noopener noreferrer"
+          >
+            @lovincyrus
+          </a>
+        </p>
+      </div>
 
       <Companies />
     </div>
+
+    <style jsx>
+      {`
+        .bg-container {
+          background-color: aliceblue;
+          padding: 20px;
+          border-radius: 4px;
+        }
+      `}
+    </style>
   </>
 );
 
