53706f2fe44c82dcdd940ce303657f0b4aeac107
Cyrus Goh <hello@lovincyrus.com>
2019-11-13 23:49:56 -0800

    Add header component

---
+++ components/header.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Head from 'next/head';
+
+import '../styles/main.css';
+
+const Header = () => (
+  <>
+    <Head>
+      <title>Cosmos & Tendermint Ecosystem</title>
+    </Head>
+
+    <div className="bg-container">
+      <h1 className="subpixel-antialiased font-sans text-lg text-gray-800 text-left font-bold text-lg md:text-3xl lg:text-4xl">
+          Cosmos &amp; Tendermint Ecosystem
+      </h1>
+      <p className="pt-1 subpixel-antialiased max-w-full text-gray-600 text-lg">
+          Note: List of active projects in the cosmos ecosystem are curated best
+          to the knowledge of the community. Everyone should maintain caution and
+          exercise due diligence when researching new projects beyond provided
+          informations.
+      </p>
+      <p className="pt-2 text-gray-500">
+          Built by
+        {' '}
+        <a
+          href="https://github.com/lovincyrus"
+          target="blank_"
+          rel="noopener noreferrer"
+        >
+            @lovincyrus
+        </a>
+      </p>
+    </div>
+
+    <style jsx>
+      {`
+        .bg-container {
+          background-color: aliceblue;
+          padding: 20px;
+          border-radius: 4px;
+        }
+      `}
+    </style>
+  </>
+);
+
+export default Header;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import Companies from '../components/companies';
+import Header from '../components/header';
 
 import '../styles/main.css';
 
@@ -11,29 +12,7 @@ const Home = () => (
     </Head>
 
     <div className="py-10 p-5 mx-auto max-w-6xl">
-      <div className="bg-container">
-        <h1 className="subpixel-antialiased font-sans text-lg text-gray-800 text-left font-bold text-4xl">
-          Cosmos &amp; Tendermint Ecosystem
-        </h1>
-        <p className="pt-1 subpixel-antialiased max-w-full text-gray-600 text-xl">
-          Note: List of active projects in the cosmos ecosystem are curated best
-          to the knowledge of the community. Everyone should maintain caution and
-          exercise due diligence when researching new projects beyond provided
-          informations.
-        </p>
-        <p className="pt-2 text-gray-500">
-          Built by
-          {' '}
-          <a
-            href="https://github.com/lovincyrus"
-            target="blank_"
-            rel="noopener noreferrer"
-          >
-            @lovincyrus
-          </a>
-        </p>
-      </div>
-
+      <Header />
       <Companies />
     </div>
 
