3d079a9144c1013ebeeb690e9b96c87626bea273
Cyrus Goh <hello@lovincyrus.com>
2019-11-16 22:51:16 -0800

    Use <Head> in index

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,7 +8,13 @@ import '../styles/main.css';
 const Home = () => (
   <>
     <Head>
-      <title>Cosmos & Tendermint Ecosystem</title>
+      <title>Cosmonauts World</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta
+        name="description"
+        content="Welcome! We're helping each other to learn more about the projects in the Cosmos and Tendermint Ecosystem."
+      />
     </Head>
 
     <div className="py-10 p-5 mx-auto max-w-6xl">
