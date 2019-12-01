f692c10fa007ff39dfcdbf80d159902c5b1b1cfc
Cyrus Goh <hello@lovincyrus.com>
2019-12-01 00:13:54 -0800

    Add placeholders

--- data/posts-manifest.js
+++ data/posts-manifest.js
@@ -1319,7 +1319,7 @@ const mapping = [
   {
     name: 'BEPSwap',
     description: 'Swap and stake BNB and BEP2 assets. BEPSwap is Binance Chain’s first decentralized finance application allowing BEP2 token holders to swap their assets, or stake them to provide liquidity to the market. Powered by THORChain technology.',
-    image: '/static/logos/peerity.png',
+    image: '/static/logos/bepswap.png',
     network: '?',
     verified: 'false',
     links: {
@@ -1331,7 +1331,7 @@ const mapping = [
   {
     name: 'DeTicket',
     description: 'verified via @Berliner1234 on TG',
-    image: '/static/logos/peerity.png',
+    image: '/static/logos/placeholder.png',
     network: '?',
     verified: 'false',
     links: {
@@ -1342,7 +1342,7 @@ const mapping = [
   {
     name: 'EVERETT',
     description: '?',
-    image: '/static/logos/peerity.png',
+    image: '/static/logos/mesg.png',
     network: '?',
     verified: 'false',
     links: {
