1e87e6502e124d3b66d5a142eb6a6a4312b970c3
Cyrus Goh <hello@lovincyrus.com>
2019-12-25 20:11:52 -0800

    comment

--- i18n.js
+++ i18n.js
@@ -16,10 +16,13 @@ const NextI18Next = require('next-i18next').default;
 // };
 
 module.exports = new NextI18Next({
+  // lng: 'en',
   defaultLanguage: 'en',
   otherLanguages: ['kr', 'cn', 'ru'],
   // localeSubpaths: localeSubpathVariations[localeSubpaths],
   // https://github.com/isaachinman/next-i18next/issues/523
   localePath: typeof window === 'undefined' ? 'public/locales' : 'locales',
+  // browserLanguageDetection: true,
+  // serverLanguageDetection: false,
   debug: false,
 });
