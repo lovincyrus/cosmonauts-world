6bbdb0b9204aec81f250960682f47dd5ca587d23
Cyrus Goh <hello@lovincyrus.com>
2019-12-25 18:08:13 -0800

    test next.config

--- next.config.js
+++ next.config.js
@@ -3,12 +3,15 @@ const withCSS = require('@zeit/next-css');
 // module.exports = withCSS({});
 
 module.exports = withCSS({
-  publicRuntimeConfig: {
-    localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string'
-      ? process.env.LOCALE_SUBPATHS
-      : 'none',
-  },
   env: {
     special: 'value',
   },
 });
+
+// module.exports = withCSS({
+//   publicRuntimeConfig: {
+//     localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string'
+//       ? process.env.LOCALE_SUBPATHS
+//       : 'none',
+//   },
+// });
