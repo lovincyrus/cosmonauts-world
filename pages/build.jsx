b62526a31b47b16e71068b7d74a179a6cf9c0f22
Cyrus Goh <hello@lovincyrus.com>
2019-12-27 17:39:21 -0800

    Add footer, move main.css to _app.js

--- pages/build.jsx
+++ pages/build.jsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 function Build() {
   return (
     <>
-      <div>
+      <div className="py-10 p-5 mx-auto max-w-6xl">
         <p>This is a Build page.</p>
       </div>
     </>
