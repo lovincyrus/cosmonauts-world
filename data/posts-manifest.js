5f0628a5ebbfcb86be7e5d5650e0f0db5358fdbd
Cyrus Goh <hello@lovincyrus.com>
2020-01-15 22:19:31 -0800

    add okchain

--- data/posts-manifest.js
+++ data/posts-manifest.js
@@ -1450,6 +1450,20 @@ const mapping = [
     },
     tags: ['cosmos sdk', 'tendermint core'],
   },
+  {
+    name: 'OKChain',
+    description: 'OKChain is a self-developed public blockchain by OKEx based on a Cosmos SDK forked chain. It is the underlying architecture of the OKB ecosystem, which is added with payment function. While maintaining the cross-chain features of Cosmos SDK, it will undergo security and efficiency optimization designed for DeFi. OKDEX will be the first DApp launched on OKChain.',
+    image: '/static/logos/okex.png',
+    network: 'development',
+    verified: 'false',
+    links: {
+      proof: 'https://medium.com/okex-blog/okex-in-support-of-community-and-ecosystem-building-1788065881a8',
+      twitter: 'https://twitter.com/OKEx',
+      chat: 'https://t.me/okexofficial_en',
+      website: 'https://www.okex.com',
+    },
+    tags: ['cosmos sdk'],
+  },
 ];
 
 module.exports = {
